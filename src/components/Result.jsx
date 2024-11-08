cc0a7226e60228ef47e38dd0afe092e2c587a734
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2024-11-08 19:19:49 +0300

    saving

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Result = () => {
   const score = localStorage.getItem('score');
-
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/home')
+  }
   return (
-    <div>
-      <Styleh1>Вы прошли тест на </Styleh1>
-      <Styleh1> {score} баллов </Styleh1>
-    </div>
+    <Styleh1>
+      <h1>Вы прошли тест на </h1>
+      <h1> {score} баллов </h1>
+      <button onClick={handleClick}>Перейти к созданию теста</button>
+    </Styleh1>
   );
 };
 
 export default Result;
-const Styleh1 = styled.h1`
+const Styleh1 = styled.div`
   padding-left: 600px;
 `
